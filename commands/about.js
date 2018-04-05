58df3faa9fdd4cf8a7dd21640e773eb864354f09
Onur Ozdemir <onur38o@hotmail.com>
2018-04-05 14:42:19 +0700

    modified about command

--- commands/about.js
+++ commands/about.js
@@ -5,11 +5,11 @@ module.exports = async ({ ctx, fetchRpc, userId, isPm, reply }) => {
 
   await reply(
     [
-      `I'm a bot (v${version}) written by Andreas Brekken (@abrkn) for tipping Bitcoin (BCH) on Telegram`,
+      `I'm a bot (v${version}) written by Andreas Brekken (@abrkn), modified by Mewnbot (@Belastingdienst) for tipping Viacoin (VIA) on Telegram`,
       'Try the /help command in a private message',
       `You can tip me and it'll go to the faucet once it's ready.`,
-      `I'm open source: https://github.com/abrkn/tipmebch`,
-      `Report bugs here: https://github.com/abrkn/tipmebch/issues`,
+      `I'm open source: https://github.com/abrkn/tipmebch / https://github.com/onurrr/tipmevia`,
+      `Report bugs here: https://github.com/onurrr/tipmevia/issues`,
       `The bot is not a wallet. Your funds will be lost if there are bugs`,
     ].join('\n')
   );
