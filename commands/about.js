0dab11a3d5aa72557eaa24bc14ae8f00654ac95e
Onur Ozdemir <onur38o@hotmail.com>
2018-04-05 16:47:39 +0700

    modified about command

--- commands/about.js
+++ commands/about.js
@@ -8,7 +8,7 @@ module.exports = async ({ ctx, fetchRpc, userId, isPm, reply }) => {
       `I'm a bot (v${version}) written by Andreas Brekken (@abrkn), modified by Mewnbot (@Belastingdienst) for tipping Viacoin (VIA) on Telegram`,
       'Try the /help command in a private message',
       `You can tip me and it'll go to the faucet once it's ready.`,
-      `I'm open source: https://github.com/abrkn/tipmebch / https://github.com/onurrr/tipmevia`,
+      `I'm open source: https://github.com/onurrr/tipmevia`,
       `Report bugs here: https://github.com/onurrr/tipmevia/issues`,
       `The bot is not a wallet. Your funds will be lost if there are bugs`,
     ].join('\n')
